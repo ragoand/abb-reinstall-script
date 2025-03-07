73d4012343291071dd161980f8a68e235b7c0088
Andrea Rago <rago93@gmail.com>
2025-03-07 08:57:25 +0100

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,7 +3,7 @@ echo "Uninstalling service...";
 dpkg -r synology-active-backup-business-linux-service;
 echo "Uninstalling driver...";
 echo "Downloading Synology Active Backup Business 2.7.1-3235...";
-wget "https://global.download.synology.com/download/Utility/ActiveBackupBusinessAgent/2.7.1-3235/Linux/x86_64/Synology%20Active%20Backup%20for%20Business%20Agent-2.7.1-3235-x64-deb.zip" -O abb.zip;
+wget "https://global.download.synology.com/download/Utility/ActiveBackupBusinessAgent/2.7.1-3235/Linux/x86_64/Synology%20Active%20Backup%20for%20Business%20Agent-2.7.1-3235-x64-deb.zip" -o abb.zip;
 echo "Extracting...";
 unzip abb.zip;
 dpkg -r synosnap;
