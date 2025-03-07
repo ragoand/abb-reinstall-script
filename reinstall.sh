5f6b2be6de2d64f054dec7f8192ffd38e3bc34a4
Andrea Rago <rago93@gmail.com>
2025-03-07 08:53:41 +0100

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -2,8 +2,12 @@
 echo "Uninstalling service...";
 dpkg -r synology-active-backup-business-linux-service;
 echo "Uninstalling driver...";
+echo "Downloading Synology Active Backup Business 2.7.1-3235...";
+wget "https://global.download.synology.com/download/Utility/ActiveBackupBusinessAgent/2.7.1-3235/Linux/x86_64/Synology%20Active%20Backup%20for%20Business%20Agent-2.7.1-3235-x64-deb.zip" -O abb.zip;
+echo "Extracting...";
+unzip abb.zip;
 dpkg -r synosnap;
 echo "Reinstalling abb...";
 ./install.run
 echo "Reconnecting..."
-abb-cli -c
+abb-cli -c --address icarus.zapt.org --username wosktools --password [PASSWORD_HERE]
