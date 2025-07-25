c54a236b488b69306fe714edd67c1718bdd9b442
Andrea Rago <rago93@gmail.com>
2025-07-25 16:37:16 +0200

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Uninstalling service...";
+echo "Removing old files..."
+rm install.run;
+rm README;
+echo "Uninstalling services...";
 dpkg -r synology-active-backup-business-linux-service;
+dokg -r synosnap;
 echo "Downloading Synology Active Backup Business 2.7.1-3235...";
 wget "http://updates.andrearago.it/abb/latest.zip" -O abb.zip;
 echo "Extracting...";
